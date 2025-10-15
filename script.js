a7bf7c0e100c0269e5afb6af409122d6549b85bd
Triktor <yosoyvictor06@gmail.com>
2025-10-15 16:29:34 +0200

    Canvas de Phaser

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function desaparece(nombre) {
 	var button = document.getElementById(nombre);
   button.style.visibility='hidden';
 }
+
+  new Phaser.Game({
+  type: Phaser.CANVAS,
+  canvas: document.getElementById('gameCanvas'),
+  width: 1280,
+  height: 720,
+});
