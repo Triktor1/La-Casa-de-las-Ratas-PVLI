3fe551fa2b6a9fcb8e1a65f24fc0884a3cb4f5a2
ossilv01 <ossilv01@ucm.es>
2025-10-24 11:49:59 +0200

    cambio rapido conflicto nombre sprite

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -14,7 +14,7 @@ export default class Level1 extends Phaser.Scene {
     preload(){
         this.load.image('loro' , 'assets/ParrotPlaceholder.png')
         this.load.image('selectButton', 'assets/selectBt.png'); 
-        this.load.image('shopButton', 'assets/shopBt.png'); 
+        this.load.image('shopButton', 'assets/shop.png'); 
 
     }
 
