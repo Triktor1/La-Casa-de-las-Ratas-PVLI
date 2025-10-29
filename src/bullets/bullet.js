c243e11e162e72121138468f824a105bf05b08ad
Ribir13 <victoa04@ucm.es>
2025-10-29 17:04:25 +0100

    Clase bullet

---
+++ src/bullets/bullet.js
@@ -0,0 +1,19 @@
+export default class Bullet extends Phaser.GameObjects.Image{
+
+    constructor(scene, x=0, y=0 , speed, damage , timeToLive , piercing ,rat, frame = 0)
+    {
+        super(scene , x , y ,texture, frame) //cosntructora sprite
+
+        this.scene.add.existing(this);
+
+        //Atributos loro
+        this.vida = 100
+        this.nombre = loroname
+        this.damage = damage
+    }
+
+
+
+
+
+}
