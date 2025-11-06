a43c0a8cb18e2f7de9d656bf2a1c0276992535a6
Ribir13 <victoa04@ucm.es>
2025-11-07 00:17:51 +0100

    TorreUI ahora incluye el precio en texto
    TorreUI ahora tiene un texto debajo del icono que muestra el precio del objeto, y se actualiza con el incremento de coste

--- src/torres/TorreUI.js
+++ src/torres/TorreUI.js
@@ -8,6 +8,10 @@ export default class TorreUI extends Phaser.GameObjects.Image {
         this.increase = 10;          //lo que sube el precio por cada compra
         this.TorreClase = TorreClase;//Clase que se va a colocar, se tendrá que importar con el js
 
+        //Texto del precio
+        this.priceText = scene.add.text(this.x, this.y + 100, `Precio: ${this.cost}`, { fontSize: '20px', color: '#ffffff', fontStyle: 'bold' }).setOrigin(0.5);
+
+
         this.setInteractive({ draggable: true });
         scene.input.setDraggable(this);
 
@@ -27,6 +31,7 @@ export default class TorreUI extends Phaser.GameObjects.Image {
                     this.scene.levelMoney -= this.cost;
                     console.log("Dinero actual", this.scene.levelMoney);
                     this.cost += this.increase;
+                    this.priceText.setText(`Precio: ${this.cost}`);
                 }
             }
             this.resetPosition();
