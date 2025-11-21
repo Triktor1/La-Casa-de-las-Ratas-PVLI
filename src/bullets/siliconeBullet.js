971c62f04d03653db1e34859719ce674622e7f3c
Ribir13 <victoa04@ucm.es>
2025-11-21 16:41:54 +0100

    la torre silocona ahora funciona pero la textura no no se porque

--- src/bullets/siliconeBullet.js
+++ src/bullets/siliconeBullet.js
@@ -1,13 +1,7 @@
 import Bullet from "../bullets/bullet.js";
 
 export default class SiliconeBullet extends Bullet {
-    constructor(scene, x, y, direction) {
-        super(scene, 0, 0, "siliconeBullet", 900, 15, direction, 800, false, true, 0, "", 1, 0);
-        this.texture = scene.textures.get("siliconeBullet");
-        this.speed = 900;
-        this.damage = 15;
-        this.timeToLive = 800;
-        
-
+    constructor(scene, x, y, texture, speed, damage, direction, timeToLive, piercing, teamRat, healValue, type, scale, frame) {
+        super(scene, x, y, texture, speed, damage, direction, timeToLive, piercing, teamRat, healValue, type, scale, frame);
     }
 }
