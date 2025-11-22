46a1a8acbcf6e11bb162bc7c959c37884c7f258e
Ribir13 <victoa04@ucm.es>
2025-11-22 18:21:51 +0100

    Bala silicona arreglada
    La bala silicona finalmente ralentiza al enemigo, ademas durante una duracion determinada
    Habria que revisar el codigo por el numero magico de la udracion del pathfollower

--- src/bullets/siliconeBullet.js
+++ src/bullets/siliconeBullet.js
@@ -3,10 +3,12 @@ import Bullet from "../bullets/bullet.js";
 export default class SiliconeBullet extends Bullet {
     constructor(scene, x, y, texture, speed, damage, direction, timeToLive, piercing, teamRat, healValue, type, scale, frame) {
         super(scene, x, y, texture, speed, damage, direction, timeToLive, piercing, teamRat, healValue, type, scale, frame);
+        this.slowAmount = 0.65; //VAlor de multiplicacion de velocidad
+        this.duration = 1500; //Duracion del slow
     }
         effectCollision(enemy){
         enemy.getDamaged(this.damage);
-        enemy.slowed(0.8); 
+        enemy.slowed(this.slowAmount, this.duration); 
         if (!this.piercing) this.destroy(); //comprobar si es perforante
     }
 
