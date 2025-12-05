18f228d968f22cdfc2170a03da3138fb7f1e94d6
ossilv01 <ossilv01@ucm.es>
2025-12-05 15:14:53 +0100

    cambio minusculo del tipo de rodadero

--- src/tropas/RataRodadero.js
+++ src/tropas/RataRodadero.js
@@ -2,7 +2,7 @@ import Tropa from "./TropaBase.js";
 
 export default class RataComecables extends Tropa {
     constructor(scene, path, x, y, texture = "rataRodadero") {
-        super(scene, path, x, y, 2, 10, 100, "G", "rataRodadero", "rataRodadero", 0, 0.3);
+        super(scene, path, x, y, 2, 10, 100, "B", "rataRodadero", "rataRodadero", 0, 0.3);
         this.setScale(0.12)
     }
 }
