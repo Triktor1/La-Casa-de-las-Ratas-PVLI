8bff59d3d36e35ed7a6943506e94d6784b630ceb
Triktor <yosoyvictor06@gmail.com>
2025-12-11 10:57:21 +0100

    cambio rápido rata sniper

--- src/torres/TorreSniper.js
+++ src/torres/TorreSniper.js
@@ -3,7 +3,7 @@ import Bullet from "../bullets/bullet.js";
 
 export default class RatSniper extends Torre {
     constructor(scene, x, y) {
-        super(scene, x, y, 3500, 5, "Sniper", "Sniper", 0, 0.5, 450);
+        super(scene, x, y, 3500, 5, "Sniper", "Sniper", 0, 0.8, 450);
 
         this.damage = 35;
         this.bulletSpeed = 3500;
